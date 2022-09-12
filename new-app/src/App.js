fab2455451367f7379b21c3b3a7e7ae4062349af
Your Name <you@example.com>
2022-09-12 23:16:22 +0300

    new app

---
+++ new-app/src/App.js
@@ -0,0 +1,18 @@
+import Message from './Message';
+import './App.css';
+
+const hello = 'Hi Dude!';
+const text = 'You proger?';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        <Message hello={hello} name={text} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ new-app/src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Message(props) {
+    return <h1>{props.hello} {props.name}</h1>;
+}
+
+export default Message;
